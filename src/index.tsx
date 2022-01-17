53e2183589535f42d42b634d69ab1dec418bd4f2
conorburke <cjburke89@gmail.com>
2022-01-17 10:26:50 +0900

    set up bundling and preview

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,9 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
     const ref = useRef<any>();
+    const iframe = useRef<any>();
     const [input, setInput] = useState('');
-    const [code, setCode] = useState('');
+    // const [code, setCode] = useState('');
 
     useEffect(() => {
         startService();
@@ -25,8 +26,12 @@ const App = () => {
 
     const onClick = async () => {
         if (ref.current) {
-            console.log('ref.current', ref.current);
+            // console.log('ref.current', ref.current);
+            // console.log(code);
 
+            iframe.current.srcdoc = html;
+
+            // bundling process
             const result = await ref.current.build({
                 entryPoints: ['index.js'],
                 bundle: true,
@@ -38,17 +43,53 @@ const App = () => {
                     global: 'window'
                 }
             })
+            
+            // don't need this anymore
+            // setCode(result.outputFiles[0].text)
 
-            setCode(result.outputFiles[0].text)
+            // pass the bundled code as a message to the iframe
+            iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+
         }
     }
+
+        // <script>
+        //     ${code}
+        // </script>
+    const html = `
+        <html>
+            <head></head>
+            <body>
+                <div id="root"></div>
+                <script>
+                    window.addEventListener('message', (event) => {
+                        console.log(event.data);
+                        try {
+                            eval(event.data);
+                        } catch (err) {
+                            const root = document.querySelector('#root');
+                            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+                            throw err;
+                            console.error(err);
+                        }
+                        
+                    }, false)
+                </script>
+            </body>
+        </html>
+    `
 
     return <div>
         <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
         <div>
             <button onClick={onClick}>Submit</button>
         </div>
-        <pre>{code}</pre>
+        {/* <pre>{code}</pre> */}
+        {/* <iframe title="iframe test" src="/test.html" sandbox="" /> */}
+        {/* use srcDoc to load locally instead of making a request with 'src' */}
+        {/* sandbox with no 'allow-same-origin' means that direct access is not allowed between iframe */}
+        {/* downside with this is we can't use localStorage and some other browser features from iframe */}
+        <iframe ref={iframe} srcDoc={html} title="iframe with srcdoc" sandbox="allow-scripts"/>
     </div>;
 }
 
